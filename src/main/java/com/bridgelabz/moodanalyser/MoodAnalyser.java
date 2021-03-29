649701d4cd84dc6109890519b775778d26580196
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-29 23:24:31 +0530

    [sai] Refactored code to take the mood message in constructor

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,6 +1,11 @@
 package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser {
+    public String message;
+    public MoodAnalyser(String msg) {
+        message = msg;
+    }
+
     public String analyseMood(String message) {
         if (message.contains("SAD")) {
             return "SAD";
